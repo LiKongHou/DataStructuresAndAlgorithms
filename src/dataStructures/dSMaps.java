041106e7259cf0ef562bbd53dc435b5fb6af81ae
wuk0ng <l3abysexy@gmail.com>
2023-04-22 10:14:49 +0700

    Update v0.1.225621042023

--- src/dataStructures/dSMaps.java
+++ src/dataStructures/dSMaps.java
@@ -9,21 +9,21 @@ public class dSMaps {
      * Trong Java, bản đồ là một đối tượng ánh xạ các khóa đến các giá trị, tương tự
      * như từ điển trong Python. Java cung cấp một số triển khai giao diện trong
      * gói, bao gồm:Mapjava.util
-     * 
-     * HashMap: Triển khai "Map" giao diện bằng cách sử dụng bảng băm. Các phần tử được
+     *
+     * HashMap: Triển khai (Implements) của Giao diện "Map" bằng cách sử dụng bảng băm. Các phần tử được
      * lưu trữ trong một vùng lưu trữ dựa trên mã băm của chúng, cung cấp hiệu suất
      * thời gian không đổi cho hầu hết các hoạt động. Việc triển khai này không đảm
      * bảo thứ tự của các khóa và giá trị.
-     * 
-     * LinkedHashMap: Mở rộng "HashMap" và duy trì một danh sách các phần tử được
+     *
+     * LinkedHashMap: Kế thừa (Extends) "HashMap" và duy trì một danh sách các phần tử được
      * liên kết
      * theo thứ tự chúng được thêm vào. Việc triển khai này cung cấp thứ tự lặp có
      * thể dự đoán được, là thứ tự mà các phần tử được thêm vào.
-     * 
-     * TreeMap: Thực hiện giao diện bằng cây đỏ-đen. Các phần tử được lưu trữ theo
+     *
+     * TreeMap: Triển khai (Implements) của Giao diện "SortedMap" bằng cây đỏ-đen. Các phần tử được lưu trữ theo
      * thứ tự sắp xếp dựa trên thứ tự tự nhiên của chúng hoặc một bộ so sánh được
      * chỉ định. Việc triển khai này cung cấp chi phí thời gian nhật ký (n) được đảm
-     * bảo cho hầu hết các hoạt động, bao gồm thêm, xóa và chứa. "SortedMap"
+     * bảo cho hầu hết các hoạt động, bao gồm thêm, xóa và chứa.
      */
 
     static Map<Integer, citysData> citys;
