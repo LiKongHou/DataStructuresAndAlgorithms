18b26cd479a4834b5bb764c88d88123f0bff0db0
wuk0ng <l3abysexy@gmail.com>
2023-04-22 10:12:14 +0700

    Update v0.1.225621042023

--- src/dataStructures/dSMaps.java
+++ src/dataStructures/dSMaps.java
@@ -1,7 +1,58 @@
 package dataStructures;
 
-public class dSMaps {
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class dSMaps {
+
+    /*
+     * Trong Java, bản đồ là một đối tượng ánh xạ các khóa đến các giá trị, tương tự
+     * như từ điển trong Python. Java cung cấp một số triển khai giao diện trong
+     * gói, bao gồm:Mapjava.util
+     * 
+     * HashMap: Triển khai "Map" giao diện bằng cách sử dụng bảng băm. Các phần tử được
+     * lưu trữ trong một vùng lưu trữ dựa trên mã băm của chúng, cung cấp hiệu suất
+     * thời gian không đổi cho hầu hết các hoạt động. Việc triển khai này không đảm
+     * bảo thứ tự của các khóa và giá trị.
+     * 
+     * LinkedHashMap: Mở rộng "HashMap" và duy trì một danh sách các phần tử được
+     * liên kết
+     * theo thứ tự chúng được thêm vào. Việc triển khai này cung cấp thứ tự lặp có
+     * thể dự đoán được, là thứ tự mà các phần tử được thêm vào.
+     * 
+     * TreeMap: Thực hiện giao diện bằng cây đỏ-đen. Các phần tử được lưu trữ theo
+     * thứ tự sắp xếp dựa trên thứ tự tự nhiên của chúng hoặc một bộ so sánh được
+     * chỉ định. Việc triển khai này cung cấp chi phí thời gian nhật ký (n) được đảm
+     * bảo cho hầu hết các hoạt động, bao gồm thêm, xóa và chứa. "SortedMap"
+     */
+
+    static Map<Integer, citysData> citys;
+
+    public static void main(String[] args) {
+        citys = new HashMap<>();
+        citys.put(1, new citysData("HaNoi"));
+        citys.put(2, new citysData("HCM"));
+        citys.put(3, new citysData("DaNang"));
+        citys.put(4, new citysData("Hue"));
+        citys.remove(4);
+
+        System.out.println(citys);
+        citys.entrySet().forEach(System.out::println);
+        citys.entrySet().forEach(x -> System.out.println("Key: " + x.getKey() + " Value: " + x.getValue()));
+        for (Map.Entry<Integer, citysData> x : citys.entrySet()) {
+            System.out.println("Key: " + x.getKey() + " Value: " + x.getValue());
+        }
+        citys.forEach((Key, citysData) -> System.out.println("Key: " + Key + "-" + citysData));
+        System.out.println("size: " + citys.size());
+        System.out.println("get: " + citys.get(4));
+        System.out.println("getOrDefault: " + citys.getOrDefault(4, new citysData("Default")));
+        System.out.println("containsKey: " + citys.containsKey(4));
+        System.out.println("keySet: " + citys.keySet());
+        System.out.println("value: " + citys.values());
+        System.out.println("entrySet: " + citys.entrySet());
+
+    }
+
+    record citysData(String citys) {
     }
 }
--- src/dataStructures/dSSets.java
+++ src/dataStructures/dSSets.java
@@ -1,6 +1,7 @@
 package dataStructures;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class dSSets {
@@ -11,15 +12,15 @@ public class dSSets {
          * Trong Java, một tập hợp là một tập hợp các phần tử duy nhất "không cho phép
          * trùng lặp". Java cung cấp một số triển khai của Set giao diện trong java.util
          * trọn gói, bao gồm:
-         * 
+         *
          * HashSet: Triển khai (Implements) của Giao diện "Set" sử dụng bảng băm. Các
          * phần tử được lưu trữ trong một vùng lưu trữ dựa trên mã băm của chúng, cung
          * cấp hiệu suất thời gian không đổi cho hầu hết các hoạt động.
-         * 
+         *
          * LinkedHashSet: Kế thừa (Extends) "HashSet" và duy trì một danh sách các phần
          * tử được liên kết theo thứ tự chúng được thêm vào. Việc triển khai này cung
          * cấp thứ tự lặp có thể dự đoán được, là thứ tự mà các phần tử được thêm vào.
-         * 
+         *
          * TreeSet: Triển khai (Implements) của Giao diện "SortedSet" bằng cách sử dụng
          * một cây đỏ-đen. Các phần tử được lưu trữ theo thứ tự sắp xếp dựa trên thứ tự
          * tự nhiên của chúng hoặc một bộ so sánh được chỉ định. Việc triển khai này
@@ -35,7 +36,10 @@ public class dSSets {
         citysSet.add(new citysInnerdSSets("DaNang"));
         citysSet.add(new citysInnerdSSets("Hue"));
         citysSet.forEach(System.out::println);
-        System.out.println(citysSet.size());
+        System.out.println("size: " + citysSet.size());
+        citysSet.clear();
+        System.out.println("isEmpty: " + citysSet.isEmpty());
+
 
     }
 
@@ -46,9 +50,29 @@ public class dSSets {
 
     static class citysInnerdSSets {
         private String citys;
-        public citysInnerdSSets(String citys){
-            this.citys =citys;
+
+        public citysInnerdSSets(String citys) {
+            this.citys = citys;
         }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            citysInnerdSSets that = (citysInnerdSSets) o;
+            return Objects.equals(citys, that.citys);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(citys);
+        }
+
+        @Override
+        public String toString() {
+            return "citysInnerdSSets{" +
+                    "citys='" + citys + '\'' +
+                    '}';
+        }
     }
 }
